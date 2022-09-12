4cbc15148cf32f6c818555d5fbf75117fc50080e
Kirill Maryukhna <kiryuha.96@gmail.com>
2022-09-12 20:15:03 +0300

    initial

---
+++ src/model.js
@@ -0,0 +1,14 @@
+import image from './assets/image.jpg'
+
+export const model = [
+    {type: 'title', value: 'Hello World'},
+    {type: 'text', value: 'some text'},
+    {
+        type: 'columns', value: [
+            '11111',
+            '22222',
+            '33333'
+        ]
+    },
+    {type: 'image', value: image}
+]
