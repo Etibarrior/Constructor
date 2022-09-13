f8e9279cb1745d5401b9998797eada3cc736bfde
Kirill Maryukhna <kiryuha.96@gmail.com>
2022-09-13 14:42:48 +0300

    Code optimization (classes implemented)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import {model} from './model'
-import {templates} from './templates'
 import './styles/main.css'
 
 const $site = document.querySelector('#site')
 
 model.forEach(block => {
-
-    const toHTML = templates[block.type]
-    if (toHTML) {
         // метод insertAdjacentHTML позволяет вставлять в определенноке место HTML 1е - куда, 2е - сам текст
-        $site.insertAdjacentHTML('beforeend', toHTML(block))
-    }
+        $site.insertAdjacentHTML('beforeend', block.toHTML())
 })
 
--- src/model.js
+++ src/model.js
@@ -1,58 +1,50 @@
 import image from './assets/image.jpg'
+import {TitleBlock, TextBlock, ColumnsBlock, ImageBlock} from './classes/blocks'
 
 const text = `Инстаграм: <a href="https://www.instagram.com/wood_loft_idea/" target="_blank">wood_loft_idea</a>`
 
 export const model = [
-    {
-        type: 'title', value: 'Конструктор-портфолио', options: {
-            tag: 'h2',
-            styles: {
-                background: 'linear-gradient(to bottom, #bd8a2d, #561106)',
-                color: '#d2eeff',
-                'text-align': 'center',
-                padding: '0.7rem'
-            }
+    new TitleBlock( 'Конструктор-портфолио', {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to bottom, #bd8a2d, #561106)',
+            color: '#d2eeff',
+            'text-align': 'center',
+            padding: '0.7rem'
         }
-    },
-    {
-        type: 'text', value: text, options: {
-            styles: {
-                background: 'linear-gradient(to top, #bd8a2d, #561106)',
-                color: '#d2eeff',
-                padding: '0.3rem',
-                'font-weight': 'bold',
-                height: '35px'
-            }
+    }),
+    new TextBlock( text, {
+        styles: {
+            background: 'linear-gradient(to top, #bd8a2d, #561106)',
+            color: '#d2eeff',
+            padding: '0.3rem',
+            'font-weight': 'bold',
+            height: '35px'
         }
-    },
-    {
-        type: 'columns', value: [
-            'Изготавление столов из дерева и эпоксидной смолы',
-            'Изготовление зеркал с подсветкой',
-            'Изготовление на заказ любых изделийиз дерева'
-        ], options: {
-            styles: {
-                background: 'linear-gradient(to bottom, #b58a3b, #e4c79f)',
-                color: '#603407',
-                'font-weight': 'bold',
-                padding: '2rem',
-                'margin-top': '1px'
-            }
+    }),
+    new ColumnsBlock( [
+        'Изготавление столов из дерева и эпоксидной смолы',
+        'Изготовление зеркал с подсветкой',
+        'Изготовление на заказ любых изделий из дерева'
+    ], {
+        styles: {
+            background: 'linear-gradient(to bottom, #b58a3b, #e4c79f)',
+            color: '#603407',
+            'font-weight': 'bold',
+            padding: '2rem',
+            'margin-top': '1px'
         }
-    },
-    {
-        type: 'image', value: image, options: {
-            styles: {
-                padding: '2px 0',
-                display: 'flex',
-                'justify-content': 'center'
-            },
-            imageStyles: {
-                width: '750px',
-                height: 'auto'
-            },
-            alt: 'Стол на фоне заката'
-
-        }
-    }
+    }),
+    new ImageBlock( image, {
+        styles: {
+            padding: '2px 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '750px',
+            height: 'auto'
+        },
+        alt: 'Стол на фоне заката'
+    })
 ]
